38032a3ca9f044e06f95752deb258fd49ab0c427
SahanaMohandoss <sahanamohandoss@gmail.com>
2018-11-10 12:50:47 +0530

    Add files via upload

--- client_reg.py
+++ client_reg.py
@@ -4,7 +4,7 @@ from flask import request
 import sqlite3
 from flask import json, session
 from validate_email import validate_email
-
+import flask
 app = Flask(__name__)
 
 Database = 'ca_firm.db'
@@ -40,6 +40,7 @@ def query_db(query, args=(), one=False):
 @app.route('/signUp',methods=['POST'])
 def signUp():
     print("here")
+    print(request.form)
     # read the posted values from the UI
     _name = request.form['username']
     _fname = request.form['firstname']
@@ -63,27 +64,51 @@ def signUp():
 
     #Check if uname exists:
     exists = [0 , 0,0,0,0] #utype , uname, emai, aadhar, pan,
-    n = query_db('SELECT * FROM client WHERE username = ?',
-                [_name], one=True)
-    exists[0] = int(request.form['usertype'])
-    if n is not None:
-    	print("This username exists")
-    	exists[0] = 1
-    e = query_db("SELECT * FROM client WHERE email_id = ?", [_email], one=True)
-    if e is not None:
-    	print("This email exists")
-    	exists[1] = 1
-    
-    a = query_db("SELECT * FROM client WHERE aadhar_no = ?", [request.form['aadhar']], one=True)
-    if a is not None:
-    	print("This aadhar exists")
-    	exists[2] = 1
-    
-    p = query_db("SELECT * FROM client WHERE pan_no = ?", [request.form['pan']], one=True)
-    if p is not None:
-    	print("This pan exists")
-    	exists[3] = 1
-
+    if(request.form['usertype']=="0"):
+        n = query_db('SELECT * FROM client WHERE username = ?',
+                    [_name], one=True)
+        #exists[0] = int(request.form['usertype'])
+        if n is not None:
+        	print("This username exists")
+        	exists[0] = 1
+        e = query_db("SELECT * FROM client WHERE email_id = ?", [_email], one=True)
+        if e is not None:
+        	print("This email exists")
+        	exists[1] = 1
+        
+        a = query_db("SELECT * FROM client WHERE aadhar_no = ?", [request.form['aadhar']], one=True)
+        if a is not None:
+        	print("This aadhar exists")
+        	exists[2] = 1
+        
+        p = query_db("SELECT * FROM client WHERE pan_no = ?", [request.form['pan']], one=True)
+        if p is not None:
+        	print("This pan exists")
+        	exists[3] = 1
+    elif(request.form['usertype']=="1"):
+        print("here in check emp")
+        n = query_db('SELECT * FROM employee WHERE username = ?',
+                        [_name], one=True)
+        #exists[0] = int(request.form['usertype'])
+        if n is not None:
+            print("This username exists")
+            exists[0] = 1
+        e = query_db("SELECT * FROM employee WHERE email_id = ?", [_email], one=True)
+        if e is not None:
+            print("This email exists")
+            exists[1] = 1      
+        print(exists)
+    else:
+        n = query_db('SELECT * FROM partner WHERE username = ?',
+                        [_name], one=True)
+        #exists[0] = int(request.form['usertype'])
+        if n is not None:
+            print("This username exists")
+            exists[0] = 1
+        e = query_db("SELECT * FROM partner WHERE email_id = ?", [_email], one=True)
+        if e is not None:
+            print("This email exists")
+            exists[1] = 1  
 
 
     print(_name, _lname, validate_email(_email) , (_password == _confpassword), flag)
@@ -95,6 +120,26 @@ def signUp():
             cols = ("username", "password", "first_name", "last_name","email_id", "company", "contact_no", "aadhar_no", "pan_no")
             vals = (_name, _password , _fname, _lname , _email , int(request.form['clientType']), _num , request.form['aadhar'] , request.form['pan'])
             insert("client" , cols, vals)
+            desc = request.form.getlist('filedesc')
+            print(desc)
+            file = request.files.getlist('files[]')
+            i = 0
+            print(file)
+            for f in file:
+                print(f)
+                cols = ("user" , "document", "description")
+                vals = (_name , f.read() , desc[i])
+                i+=1
+                insert("client_files" , cols, vals)
+            print(file)
+            print(request.form)
+            print(request.files)
+
+
+
+
+
+
             print("done")
 	    	#print(rows1)
         elif(request.form['usertype']=="1"):
@@ -125,30 +170,62 @@ def signUp():
 @app.route('/logIn',methods=['POST'])
 def logIn():
     #print(request.form)
+    t = request.form['typeofuser']
     name = request.form['uname']
     password = request.form['pwd']
     print("In log in")
-    print(name , password)
-    a = query_db("SELECT * FROM client WHERE username = ?", [name], one=True)
-    print("Values" , a)
-    if(a is None):
-        return json.dumps({'status':0})
-    else:
-        if(a[1]==password):
-            
-            #render_template("ClientHome.html")
-            session['username'] = name
-            return json.dumps({'status':1 , 'type':int(a[5]) })
-
+    print(name , password , t)
+    if(t=="0"):
+        a = query_db("SELECT * FROM client WHERE username = ?", [name], one=True)
+        print("Values" , a)
+        if(a is None):
+            return json.dumps({'status':0})
         else:
-            return json.dumps({'status':2})
+            if(a[1]==password):
+                
+                #render_template("ClientHome.html")
+                session['username'] = name
+                return json.dumps({'status':1 , 'type':int(a[5]) })
+
+            else:
+                return json.dumps({'status':2})
+    elif(t=="1"):
+        a = query_db("SELECT * FROM employee WHERE username = ?", [name], one=True)
+        print("Values" , a)
+        if(a is None):
+            return json.dumps({'status':0})
+        else:
+            if(a[1]==password):
+                
+                #render_template("ClientHome.html")
+                session['username'] = name
+                return json.dumps({'status':1 , 'type':int(t) })
+
+            else:
+                return json.dumps({'status':2})
+    else:
+        a = query_db("SELECT * FROM client WHERE partner = ?", [name], one=True)
+        print("Values" , a)
+        if(a is None):
+            return json.dumps({'status':0})
+        else:
+            if(a[1]==password):
+                
+                #render_template("ClientHome.html")
+                session['username'] = name
+                return json.dumps({'status':1 , 'type':int(t) })
+
+            else:
+                return json.dumps({'status':2})
 
 
 @app.route('/clientHome')
 def clientHome():
     
     name = session['username']
-    return render_template("ClientHome.html", username=name)
+    s = query_db("SELECT * FROM service WHERE user = ?", [name])
+    print(s)
+    return render_template("ClientHome.html", username=name, items = s)
 
 
 
@@ -157,7 +234,51 @@ def logout():
    # remove the username from the session if it is there
    session.pop('username', None)
    
-   return render_template("ClientRegister.html")
+   return render_template("ClientRegister.html"  )
+
+
+def cover_str(cvr):
+    cvr = request.files['cover']
+    if cvr and allowed_file(cvr.filename):
+        filename = secure_filename(cvr.filename)
+        cvr = cvr.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return cvr
+
+@app.route('/submitRequest',methods=['POST'])
+def submitRequest():
+    print("IN SUBMIT")
+    
+    username =session['username']
+    description = request.form['description']
+    service = request.form['service']
+
+    cols = ("user", "type_of_service", "description" , "quotation" , "accepted")
+    vals = (username , service , description, 0.0 , 0)
+    print(username , service, description)
+    insert("service" , cols, vals)
+
+    token = query_db("SELECT * FROM service WHERE user = ? AND type_of_service LIKE ? AND description LIKE ?", [username , service, description], one=True)
+    token = token[1]
+    print(token)
+    print("here in submit")
+    print(service, description)
+    desc = request.form.getlist('filedesc')
+    print(desc)
+    file = request.files.getlist('files[]')
+    i = 0
+    for f in file:
+        print(f)
+        cols = ("token" , "document", "description")
+        vals = (token , f.read() , desc[i])
+        i+=1
+        insert("service_docs" , cols, vals)
+    print(file)
+    print(request.form)
+    print(request.files)
+    return json.dumps({"status":0, "token":token})    
+
+
+
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
