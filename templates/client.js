b4bb313b3547f0ffe011bb8a1e5b09aa42220d33
Sharnam Khurana <23208029+SharnamK@users.noreply.github.com>
2018-11-16 01:56:37 +0530

    updated client side javascript

--- templates/client.js
+++ templates/client.js
@@ -1,6 +1,9 @@
 var data; 
 
-/*This is client side javascript. It will listen for any click on notification link prevent the default action, send xhr async request to nodejs server asking to scrape the data and then on recieving the data store the data in localstorage and change the page to another page which was supposed to happen on clicking the hyperlink.*/
+/*This is client side javascript. 
+It will listen for any click on notification link prevent the default action,
+send xhr async request to nodejs server asking to scrape the data and then on recieving the data,
+store the data in localstorage and change the page to another page which was supposed to happen on clicking the hyperlink.*/
 
 var obj = {
     xhr: new XMLHttpRequest(),
