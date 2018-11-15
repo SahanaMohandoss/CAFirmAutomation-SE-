642d31c2bf8d45db68f3468338fe64b837e31514
BolshackLancer <shardulsingh1@gmail.com>
2018-11-16 02:56:45 +0530

    Fixed Employee complete request function

--- client_reg.py
+++ client_reg.py
@@ -487,7 +487,7 @@ def submitRequest():
 def EmployeeHome():
     #testtext = session['testtext']
     name = session['username'] 
-    s = query_db("SELECT token_no ,current_timestamp, quotation, type_of_service,  status_for_client, emp, estimated_time_of_completion  FROM service  \
+    s = query_db("SELECT token_no ,current_timestamp, quotation, type_of_service,  status_for_client, emp, ESTIMATED_HOURS  FROM service  \
         JOIN service_status ON token_no = service_status.token \
         JOIN service_allocation ON token_no = service_allocation.token \
          WHERE emp = ?", [name])
@@ -508,9 +508,9 @@ def EmployeeHome():
 
     print(invoice)
 
-    rows4 = query_db("select S.TOKEN_NO,S.DESCRIPTION,S.TYPE_OF_SERVICE, S.current_timestamp, estimated_time_of_completion FROM SERVICE S JOIN SERVICE_STATUS SS ON S.TOKEN_NO = SS.TOKEN JOIN SERVICE_ALLOCATION ON S.TOKEN_NO = SERVICE_ALLOCATION.TOKEN WHERE SS.COMPLETED=0 AND SERVICE_ALLOCATION.EMP = ?", [name])
+    rows4 = query_db("select S.TOKEN_NO,S.DESCRIPTION,S.TYPE_OF_SERVICE, S.current_timestamp, ESTIMATED_HOURS FROM SERVICE S JOIN SERVICE_STATUS SS ON S.TOKEN_NO = SS.TOKEN JOIN SERVICE_ALLOCATION ON S.TOKEN_NO = SERVICE_ALLOCATION.TOKEN WHERE SS.COMPLETED=0 AND SERVICE_ALLOCATION.EMP = ?", [name])
     
-    rows5 = query_db("select S.TOKEN_NO,S.DESCRIPTION,S.TYPE_OF_SERVICE, S.current_timestamp, estimated_time_of_completion FROM SERVICE S JOIN SERVICE_STATUS SS ON S.TOKEN_NO = SS.TOKEN JOIN SERVICE_ALLOCATION ON S.TOKEN_NO = SERVICE_ALLOCATION.TOKEN WHERE SS.COMPLETED=1 AND SS.VERIFIED=0 AND SERVICE_ALLOCATION.EMP = ?", [name])
+    rows5 = query_db("select S.TOKEN_NO,S.DESCRIPTION,S.TYPE_OF_SERVICE, S.current_timestamp, ESTIMATED_HOURS FROM SERVICE S JOIN SERVICE_STATUS SS ON S.TOKEN_NO = SS.TOKEN JOIN SERVICE_ALLOCATION ON S.TOKEN_NO = SERVICE_ALLOCATION.TOKEN WHERE SS.COMPLETED=1 AND SS.VERIFIED=0 AND SERVICE_ALLOCATION.EMP = ?", [name])
     return render_template("EMPLOYEEV2.html", username=name, items = s, messages= m, files=files, invoice=invoice,rows4=rows4, rows5=rows5)
 
 
@@ -522,8 +522,13 @@ def complete():
         con = sql.connect(Database)
         con.row_factory = sql.Row
         cur = con.cursor()
-        print(ReceivedValues["token"])
-        exe = query_db('UPDATE SERVICE_STATUS SET COMPLETED = 1 WHERE TOKEN = ?', [ReceivedValues["token"]])
+        print(ReceivedValues.keys())
+        # print(ReceivedValues["token"])
+        checkexistence = query_db('SELECT * FROM SERVICE_STATUS WHERE TOKEN = ?', [ReceivedValues["token"]] )
+        if checkexistence is None:
+            query_db("INSERT INTO SERVICE_STATUS (TOKEN,COMPLETED,VERIFIED,status_for_partner) values(?,1,0,'NO COMMENTS')", [ReceivedValues["token"]])
+        else:
+            exe = query_db('UPDATE SERVICE_STATUS SET COMPLETED = 1 WHERE TOKEN = ?', [ReceivedValues["token"]])
         con.close()
         return "done"
 
@@ -539,6 +544,9 @@ def partnerstatus():
         con.row_factory = sql.Row
         cur = con.cursor()
         print(ReceivedValues["token"])
+        checkexistence = query_db('SELECT * FROM SERVICE_STATUS WHERE TOKEN = ?', [ReceivedValues["token"]] )
+        if checkexistence is None:
+            query_db("INSERT INTO SERVICE_STATUS (TOKEN,COMPLETED,VERIFIED,status_for_partner) values(?,0,0,'NO COMMENTS')", [ReceivedValues["token"]])
         exe = query_db('UPDATE SERVICE_STATUS SET STATUS_FOR_PARTNER = ? WHERE TOKEN = ?',[ReceivedValues["status"]], [ReceivedValues["token"]])
         con.close()
         return "done"
@@ -552,6 +560,9 @@ def clientstatus():
         con.row_factory = sql.Row
         cur = con.cursor()
         print(ReceivedValues["token"])
+        checkexistence = query_db('SELECT * FROM SERVICE_STATUS WHERE TOKEN = ?', [ReceivedValues["token"]] )
+        if checkexistence is None:
+            query_db("INSERT INTO SERVICE_STATUS (TOKEN,COMPLETED,VERIFIED,status_for_partner) values(?,0,0,'NO COMMENTS')", [ReceivedValues["token"]])
         exe = query_db('UPDATE SERVICE_STATUS SET STATUS_FOR_CLIENT = ? WHERE TOKEN = ?',[ReceivedValues["status"]], [ReceivedValues["token"]])
         con.close()
         return "done"
