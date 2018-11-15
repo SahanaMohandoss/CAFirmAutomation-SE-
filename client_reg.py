ddc6dc9b195a5e90616b8445dd2513bf88eb65d5
BolshackLancer <shardulsingh1@gmail.com>
2018-11-16 05:02:51 +0530

    Fixed bug: return to partner now works

--- client_reg.py
+++ client_reg.py
@@ -497,23 +497,19 @@ def EmployeeHome():
         JOIN service_status ON token_no = service_status.token \
         JOIN service_allocation ON token_no = service_allocation.token \
          WHERE emp = ?", [name])
-    print(s)
     m = query_db("SELECT sender , current_timestamp ,message  FROM messages  \
          WHERE recepient = ? ORDER BY current_timestamp DESC", [name])
-    print(m)
-    files = query_db("SELECT completed_service_docs.token , filename , completed_service_docs.description   FROM completed_service_docs  \
-                JOIN service ON completed_service_docs.token = service.token_no \
-                JOIN service_allocation ON completed_service_docs.token = service_allocation.token \
+    files = query_db("SELECT service_docs.token , filename , service_docs.description   FROM service_docs  \
+                JOIN service ON service_docs.token = service.token_no \
+                JOIN service_allocation ON service_docs.token = service_allocation.token \
          WHERE emp = ? ", [name])
-    print(files)
     invoice = query_db("SELECT completed_service_invoice.token, generated_by , current_timestamp ,filename, invoice_amount  FROM completed_service_invoice  \
          JOIN service ON completed_service_invoice.token = token_no \
          JOIN service_allocation ON service_allocation.token = completed_service_invoice.token  \
          WHERE service_allocation.EMP = ? ORDER BY current_timestamp DESC", [name])
 
 
-    print(invoice)
-
+    print(files)
     rows4 = query_db("select S.TOKEN_NO,S.DESCRIPTION,S.TYPE_OF_SERVICE, S.current_timestamp, ESTIMATED_HOURS FROM SERVICE S JOIN SERVICE_STATUS SS ON S.TOKEN_NO = SS.TOKEN JOIN SERVICE_ALLOCATION ON S.TOKEN_NO = SERVICE_ALLOCATION.TOKEN WHERE SS.COMPLETED=0 AND SERVICE_ALLOCATION.EMP = ?", [name])
     
     rows5 = query_db("select S.TOKEN_NO,S.DESCRIPTION,S.TYPE_OF_SERVICE, S.current_timestamp, ESTIMATED_HOURS FROM SERVICE S JOIN SERVICE_STATUS SS ON S.TOKEN_NO = SS.TOKEN JOIN SERVICE_ALLOCATION ON S.TOKEN_NO = SERVICE_ALLOCATION.TOKEN WHERE SS.COMPLETED=1 AND SS.VERIFIED=0 AND SERVICE_ALLOCATION.EMP = ?", [name])
@@ -553,7 +549,9 @@ def partnerstatus():
         checkexistence = query_db('SELECT * FROM SERVICE_STATUS WHERE TOKEN = ?', [ReceivedValues["token"]] )
         if checkexistence is None:
             query_db("INSERT INTO SERVICE_STATUS (TOKEN,COMPLETED,VERIFIED,status_for_partner) values(?,0,0,'NO COMMENTS')", [ReceivedValues["token"]])
-        exe = query_db('UPDATE SERVICE_STATUS SET STATUS_FOR_PARTNER = ? WHERE TOKEN = ?',[ReceivedValues["status"]], [ReceivedValues["token"]])
+        exe = query_db('UPDATE SERVICE_STATUS SET STATUS_FOR_PARTNER = ? WHERE TOKEN = ?',[ReceivedValues["status"], ReceivedValues["token"]])
+        query_db('UPDATE SERVICE SET ALLOCATED=0 WHERE TOKEN_NO = ?', [ReceivedValues["token"]])
+        query_db('DELETE FROM SERVICE_ALLOCATION WHERE TOKEN = ?', [ReceivedValues["token"]])
         con.close()
         return "done"
 
