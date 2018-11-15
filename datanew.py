a812e7206826941a1f47d35b2c90033319b3a4b6
Sharnam Khurana <23208029+SharnamK@users.noreply.github.com>
2018-11-16 02:06:11 +0530

    Dummy data for database.py

---
+++ datanew.py
@@ -0,0 +1,139 @@
+EMPLOYEE
+
+empdata1 = ('emp1', '123', 'bob', '789', 'gg', 1238, 'sharnamkhurana@yahoo.com')
+empdata2 = ('emp2', '124', 'alu', '234', 'ali', 1239, 'shardulsingh1@gmail.com')
+empdata3 = ('emp3', '125', 'bbb', '955', 'xxx', 1240, 'shivassv97@gmail.com')
+empdata4 = ('emp4', '126', 'aaa', '957', 'djk', 1241, 'sakshammanwati98@gmail.com')
+empdata5 = ('emp5', '127', 'fhj', '854', 'khl', 1242, 'sharnam.khurana2@gmail.com')
+empdata6 = ('emp6', '128', 'khk', '879', 'kjg', 1243, 'simrandhinwa@gmail.com')
+empdata7 = ('emp7', '129', 'iuh', '859', 'dgy', 1244, 'smruti297@gmail.com')
+
+SERVICE
+
+servdata1 = ('pqr',1,'GST Registration',45.2,'some work desc',0,0,34,'2018-06-06','solid work', 'pos')
+servdata2 = ('mhk',2,'Projections',32.2,'some work desc again',1,0,12,'2017-07-07','fast n good work', 'pos')
+servdata3 = ('fjg',4,'Internal Audit',50.0,'some work desc again',1,0,80,'2016-05-05','slow work', 'neg')
+servdata4 = ('jty',5,'Certification',40.0,'some work desc',0,0,40,'2016-07-08','good work', 'pos')
+servdata5 = ('jtj',6,'Audit',35.5,'some work desc',1,1,39,'2010-08-08','fast work', 'pos')
+servdata6 = ('kik',7,'TDS',80.0,'some work desc again',1,0,20,'2010-08-06','pricey', 'neg')
+servdata7 = ('lol',8,'Accounting',35.2,'some work desc again',1,1,40,'2011-09-09','good work', 'pos')
+
+PARTNER
+
+partner1 = ('abc','123','cba','hgfj',1234,35678,'shivassv97dev@gmail.com')
+partner2 = ('ggj','678','fed','asdf',2345,89790,'shardulsingh1@gmail.com')
+partner3 = ('def','456','fde','abcd',4534,34534,'sharnamkhurana@yahoo.com')
+partner4 = ('hij','789','jih','efgh',2342,47574,'sakshammanwati98@gmail.com')
+partner5 = ('klm','135','mlk','ijkl',6564,86788,'simrandhinwa@gmail.com')
+partner6 = ('nop','147','pon','mnop',7888,23522,'smruti297@gmail.com')
+partner7 = ('qrs','357','srq','qrst',8678,44444,'sharnam.khurana2@gmail.com')
+
+CLIENT
+
+clidata1 = ('pqr','567','vgj','dgf','smruti297@gmail.com',1,67865,499118455246,'AAAPL1234C')
+clidata2 = ('mhk','897','ram','khk','sharnam.khurana2@gmail.com',0,89789,468934560934,'JKHGL7846N')
+clidata3 = ('fjg','234','hgf','jlk','shivassv97dev@gmail.com',1,89564,438934560934,'GHREL7846N')
+clidata4 = ('jty','645','asd','tyu','sakshammanwati98@gmail.com',1,46789,238934560934,'KKHHL7846N')
+clidata5 = ('jtj','878','uyi','wew','shardulsingh1@gmail.com',0,89767,228934560934,'RTRTL7846N')
+clidata6 = ('kik','798','dsf','wef','simrandhinwa@gmail.com',1,89456,118934560934,'DFGDL7846N')
+clidata7 = ('lol','233','jty','sdf','sharnamkhurana@yahoo.com',0,83489,778934560934,'YTJRL7846N')
+
+COMPLETED SERVICE DOCS
+
+com_serv1 = (12,'a.pdf','test1.pdf','desc1')
+com_serv2 = (15,'c.pdf','test2.pdf','desc2')
+com_serv3 = (15,'d.pdf','test3.pdf','desc3')
+com_serv4 = (15,'e.pdf','test4.pdf','desc4')
+com_serv5 = (15,'f.pdf','test5.pdf','desc5')
+com_serv6 = (15,'g.pdf','test6.pdf','desc6')
+com_serv7 = (15,'h.pdf','test7.pdf','desc7')
+
+COMPLETED SERVICE INVOICE
+
+invoice_1 = (13,'abc','doc1.pdf','inva.pdf',1500, '2018-12-12')
+invoice_2 = (16,'ggj','doc2.pdf','invb.pdf',800, '2017-09-09')
+invoice_3 = (16,'ggj','doc2.pdf','invb.pdf',800, '2015-05-05')
+invoice_4 = (16,'ggj','doc2.pdf','invb.pdf',800, '2011-01-01')
+invoice_5 = (16,'ggj','doc2.pdf','invb.pdf',800, '2009-01-01')
+invoice_6 = (16,'ggj','doc2.pdf','invb.pdf',800, '2016-06-06')
+invoice_7 = (16,'ggj','doc2.pdf','invb.pdf',800 '2014-08-08')
+
+CLIENT_FILES
+
+cli_files1 = ('pqr','a.pdf','abc.pdf','desc1')
+cli_files2 = ('mhk','b.pdf','def.pdf','desc2')
+cli_files3 = ('fjg','c.pdf','ghi.pdf','desc3')
+cli_files4 = ('jty','d.pdf','jkl.pdf','desc4')
+cli_files5 = ('jtj','e.pdf','mno.pdf','desc5')
+cli_files6 = ('kik','f.pdf','pqr.pdf','desc6')
+cli_files7 = ('lol','g.pdf','stu.pdf','desc7')
+
+REMINDER
+
+rem1 = (1,'sha','abc','2018-12-12', '2017-12-12','reminder1', 'sharnamkhurana@yahoo.com',0)
+rem2 = (2,'rna','ggj','2017-10-12', '2016-08-12','reminder2', 'smruti297@gmail.com',0)
+rem3 = (3,'mak','def','2016-11-12', '2015-07-02','reminder3', 'shivassv97dev@gmail.com',0)
+rem4 = (4,'hur','hij','2011-02-12', '2010-02-22','reminder4', 'sakshammanwati98@gmail.com',0)
+rem5 = (5,'ana','klm','2013-02-02', '2012-07-12','reminder5', 'shardulsingh1@gmail.com',0)
+rem6 = (6,'smr','nop','2014-07-02', '2013-03-16','reminder6', 'simrandhinwa@gmail.com',0)
+rem7 = (7,'uti','qrs','2016-02-07', '2015-09-17','reminder7', 'sharnam.khurana2@gmail.com',0)
+
+MESSAGES
+
+mes1 = ('sharnam','shakti','m1','2018-01-01',1)
+mes2 = ('shiv','smruti','m2','2017-06-08',2)
+mes3 = ('sahana','shakti','m3','2016-11-11',4)
+mes4 = ('sid','saksham','m4','2010-06-07',5)
+mes5 = ('smruti','shardul','m5','2011-11-05',6)
+mes6 = ('shardul','simran','m6','2013-03-04',7)
+mes7 = ('simran','sharnam','m7','2008-06-05',8)
+
+REQUEST FILES
+
+req1 = (1,'mes1','2018-02-03')
+req2 = (2,'mes2','2018-02-05')
+req3 = (4,'mes3','2018-04-05')
+req4 = (5,'mes4','2014-02-03')
+req5 = (6,'mes5','2013-12-03')
+req6 = (7,'mes6','2010-02-03')
+req7 = (8,'mes7','2016-02-06')
+
+SERVICE_DOCS
+
+serv1 = (1,'a.pdf','abc.pdf','desc1')
+serv2 = (2,'b.pdf','def.pdf','desc2')
+serv3 = (4,'c.pdf','ghi.pdf','desc3')
+serv4 = (5,'d.pdf','jkl.pdf','desc4')
+serv5 = (6,'e.pdf','mno.pdf','desc5')
+serv6 = (7,'f.pdf','pqr.pdf','desc6')
+serv7 = (8,'g.pdf','stu.pdf','desc7')
+
+SERVICE ALLOCATION
+
+serv_alloc1 = (1,'emp1', '2018-02-03','abc')
+serv_alloc2 = (2,'emp2', '2017-01-02','ggj')
+serv_alloc3 = (4,'emp3', '2013-01-02','def')
+serv_alloc4 = (5,'emp4', '2016-01-07','hij')
+serv_alloc5 = (6,'emp5', '2017-08-06','klm')
+serv_alloc6 = (7,'emp6', '2016-09-08','nop')
+serv_alloc7 = (8,'emp7', '2017-01-09','qrs')
+
+SERVICE_STATUS
+
+serv_stat1 = (1,1,0,'good','incomplete','done', '2018-05-06')
+serv_stat2 = (2,0,1,'bad','complete','undone', '2015-03-06')
+serv_stat3 = (4,1,0,'good','incomplete','done', '2016-07-06')
+serv_stat4 = (5,0,0,'bad','complete','undone', '2013-05-12')
+serv_stat5 = (6,0,1,'good','incomplete','done', '2011-05-07')
+serv_stat6 = (7,0,0,'bad','complete','undone', '2010-02-03')
+serv_stat7 = (8,1,1,'good','incomplete','done', '2014-04-07')
+
+COMPLETE_SERVICE_SUMMARY
+
+com_sum1 = (1,'summary1')
+com_sum2 = (2,'summary2')
+com_sum3 = (4,'summary3')
+com_sum4 = (5,'summary4')
+com_sum5 = (6,'summary5')
+com_sum6 = (7,'summary6')
+com_sum7 = (8,'summary7')
