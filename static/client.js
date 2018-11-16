a8efdf5731c6b073a6ccd647129a1b82fe114ba1
Sharnam Khurana <23208029+SharnamK@users.noreply.github.com>
2018-11-16 11:22:41 +0530

    (notif) client side

---
+++ static/client.js
@@ -0,0 +1,32 @@
+var data; 
+
+/*This is client side javascript. 
+It will listen for any click on notification link prevent the default action, 
+send xhr async request to nodejs server asking to scrape the data and then on receiving the data,
+store the data in localstorage and change the page to another page which was supposed to happen on clicking the hyperlink.*/
+
+var obj = {
+    xhr: new XMLHttpRequest(),
+    linkclick: function(e){
+        e.preventDefault();
+        var update = function(response){
+            data = response;
+            localStorage.setItem('Data_for_table', data);
+            window.location.href= "notification.html";            
+        }
+        obj.getData(update);
+    },
+    
+    getData: function(callback){
+        this.xhr.onreadystatechange= function(){
+            if(this.readyState==4 && this.status==200){
+                var res=this.responseText;
+                console.log(res);
+                callback.apply(this, [res]);
+            }
+        };
+        this.xhr.open("GET","http://localhost:7000",true);
+        this.xhr.send();
+        
+    }
+}
